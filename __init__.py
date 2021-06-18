6ea9ad8d12b09d70e0bbdd28b98dc26adf6e5995
Andre Angel <andre.angel@redytec.local>
2021-06-18 13:50:57 -0500

    basics

--- __init__.py
+++ __init__.py
@@ -1,15 +1,12 @@
 {
-    'name': "Custom Account View",
+    'name': 'Custom Account View',
     'author': 'RED Y TEC',
-    'version': '14.0.1.0.0',
-    'summary': 'Changes views on',
-    'sequence': 20,
+    'version': '1.0',
+    'summary': 'Changes views on account module',
     'description': """Cambia la vista de formulario y la vista de árbol para mostrar referencias en el módulo de facturación""",
-    'category': '',
+    'category': 'Productivity',
     'website': 'http://www.redytec.com',
-    'depends': [
-
-    ],
+    'depends': [],
     'data': [
 
     ],
