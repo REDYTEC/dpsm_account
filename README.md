abe229f5919c3c7d9a60a075ec5c1636f2283e54
Andre Angel <andre.angel@redytec.local>
2021-06-18 13:26:13 -0500

    basics

--- README.md
+++
@@ -1,1 +0,0 @@
-# dpsm_account
---
+++ __init__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Custom Account View",
+    'author': 'RED Y TEC',
+    'version': '14.0.1.0.0',
+    'summary': 'Changes views on',
+    'sequence': 20,
+    'description': """Cambia la vista de formulario y la vista de árbol para mostrar referencias en el módulo de facturación""",
+    'category': '',
+    'website': 'http://www.redytec.com',
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto:install': False,
+}
