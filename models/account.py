6bab52ccdf52959b41bad2953c4b5042b3b19c61
Andre Angel <andre.angel@redytec.local>
2021-06-29 11:07:41 -0500

    I want to dead

--- models/account.py
+++ models/account.py
@@ -2,8 +2,8 @@
 from odoo import api, fields, models, _, tools
 
 
-class SaleOrder(models.Model):
-    _inherit = "account.move"
+# class SaleOrder(models.Model):
+#     _inherit = "account.move"
 
 
 class SaleInherit(models.Model):
---
+++ models/ref_inherit.py
@@ -0,0 +1,5 @@
+from odoo import api, fields, models, _, tools
+
+
+class SaleOrder(models.Model):
+    _inherit = "account.move"
