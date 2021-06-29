11ff2e81a2493eaec6900b03065def98a9fd9770
Andre Angel <andre.angel@redytec.local>
2021-06-29 10:56:50 -0500

    I want to dead

--- models/account.py
+++ models/account.py
@@ -9,18 +9,6 @@ class SaleOrder(models.Model):
 class SaleInherit(models.Model):
     _inherit = "sale.order"
 
-    pay_rate = fields.Many2one('product.pricelist')
-    nombre = fields.Char()
-    banco = fields.Char()
-    cuenta = fields.Char()
-    cable = fields.Char()
-    rfc = fields.Char()
+    psm = fields.Boolean(string='Cuentas PSM')
+    zk = fields.Boolean(string='Cuentas ZK')
 
-    def table_condition(self):
-        if self.pay_rate == 'Tarifa de Distribuidores (MXN)' or self.pay_rate.name == 'Tarifa de Distribuidores (USD)':
-            self.nombre = 'ZKTECO LATAM S.A. DE C.V.'
-        else:
-            self.nombre = ''
-
-    # psm = fields.Boolean(string='Cuentas PSM')
-    # zk = fields.Boolean(string='Cuentas ZK')
