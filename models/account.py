4e127ac0ea519d788c5ace7774b7ee87516b488b
Andre Angel <andre.angel@redytec.local>
2021-07-05 11:18:41 -0500

    moving boolean fields

--- models/account.py
+++ models/account.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models, _, tools
 
 
 class SaleInherit(models.Model):
-    _inherit = "sale.order"
+    _inherit = "sale.order", "sale.order.line"
 
     psm = fields.Boolean(string='Cuentas PSM')
     zk = fields.Boolean(string='Cuentas ZK')
